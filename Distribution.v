0c5185fdb37baeb5f807c62ef87fb0460ed15ca3
Vadim Pushtaev <pushtaev.vm@gmail.com>
2024-11-19 20:23:20 +0200

    distribution_pick()

--- Distribution.v
+++ Distribution.v
@@ -114,6 +114,32 @@ Fixpoint distribution_to_labels {LT : Type} (d : distribution): list LT :=
     label :: distribution_to_labels tail
   end.
 
+Fixpoint _distribution_pick
+    {LT : Type}
+    (probs : list Q)
+    (labels : list LT)
+    (err : LT)
+    (random : Q)
+    : LT :=
+  match probs with
+  | [] => err
+  | prob :: probs_tail =>
+    match labels with
+    | [] => err
+    | label :: labels_tail =>
+      if Qle_bool random prob && negb (Qeq_bool random prob)
+        then label
+        else _distribution_pick probs_tail labels_tail err (random - prob)
+    end
+   end.
+
+Definition distribution_pick {LT : Type} (d : distribution) (err : LT) (random : Q) : LT :=
+  _distribution_pick
+    (distribution_to_probs d)
+    (distribution_to_labels d)
+    err
+    random.
+
 Definition list_q_sum (lst : list Q) :=
   fold_right Qplus 0 lst.
 
@@ -338,7 +364,20 @@ Proof.
     reflexivity.
 Qed.
 
+(* Probs, labels, pick! *)
+Definition __d := (
+  distributions_mult
+    (Single 7%nat 1 Q_apart_0_1)
+    (uniform_distribution 5 (fun n => n))
+    (fun x y => x * y)%nat
+).
+Definition __probs := (distribution_to_probs __d).
+Compute __probs.
+Definition __labels := (distribution_to_labels __d).
+Compute __labels.
+Compute (distribution_pick __d 500%nat (59#100)%Q).
 
+(***)
 Compute (
   distribution_to_probs
   (distributions_mult (Single 7%nat 1 Q_apart_0_1) (uniform_distribution 5 (fun n => n)) (fun x y => x * y)%nat)
